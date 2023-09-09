99cf5eb245f34977383e014c7388407b5dba205f
Cena Abachi <devloger.io@gmail.com>
2023-09-09 22:22:42 +0400

    Update createWorld.js

--- server/utils/createWorld.js
+++ server/utils/createWorld.js
@@ -1,3 +1,4 @@
+// Made by Cena Abachi Known as Devlogerio, find me on Youtube, Instagram, and Github: Devlogeiro LinkedIn: Cena Abachi, devloger.io@gmail.com
 const SimplexNoise = require('simplex-noise');
 const {toRadians, toDegrees} = require('./functions');
 var vagina = {};
