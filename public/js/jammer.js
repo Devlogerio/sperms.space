8ceb65ad3e31eb52ae00344eaf38b598e1081179
Cena Abachi <devloger.io@gmail.com>
2023-09-09 22:26:12 +0400

    Update jammer.js

--- public/js/jammer.js
+++ public/js/jammer.js
@@ -1,3 +1,4 @@
+// Made by Cena Abachi Known as Devlogerio, find me on Youtube, Instagram, and Github: Devlogeiro LinkedIn: Cena Abachi, devloger.io@gmail.com
   // This is th class or object of the sperm
   var Jammer = function(initPack){
     var self = {};
