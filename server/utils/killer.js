80ec6c75e52ff88ab1629a49dfac2a7dbc76d90e
Cena Abachi <devloger.io@gmail.com>
2023-09-09 22:23:51 +0400

    Update killer.js

--- server/utils/killer.js
+++ server/utils/killer.js
@@ -1,3 +1,4 @@
+// Made by Cena Abachi Known as Devlogerio, find me on Youtube, Instagram, and Github: Devlogeiro LinkedIn: Cena Abachi, devloger.io@gmail.com
 const Victor = require('victor');
 const {Entity} = require('./entity');
 const {randomBetweenTwoNumbers, limitVector} = require('./functions');
